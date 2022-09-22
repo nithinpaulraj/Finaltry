9bfd77f7f07692d3e87f653324b2db7af2c07018
nithinpaulraj <paulnithin137@gmail.com>
2022-09-22 22:13:47 +0530

    done

---
+++ src/org/Git/Task.java
@@ -0,0 +1,15 @@
+package org.Git;
+
+public class Task {
+	private void details() {
+	String name ="nithin";
+	int age=24;
+System.out.println(name);
+	}
+public static void main(String[] args) {
+Task t1 = new Task();
+t1.details();
+
+
+}
+}
---
+++ src/org/Git/Task2.java
@@ -0,0 +1,8 @@
+package org.Git;
+
+public class Task2 {
+	public static void main(String[] args) {
+		System.out.println("Git domain");
+	}
+
+}
